5fec61a8f0ed20ddcb62562a8cb5d707f124e116
Matt Obert on Windows <matt@hotwebmatter.com>
2019-09-01 11:46:19 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using static System.Console;
+
+namespace AsciiBanner
+{
+    class Program
+    {
+        static void RepeatChar(string character, int reps)
+        {
+            for (int i = 0; i < reps; i++)
+            {
+                Write(character);
+            }
+        }
+        static void Main(string[] args)
+        {
+            // variable declaration
+            const int bannerWidth = 54;
+            const string name = "Matthew Obert";
+            string date = DateTime.Now.ToString("MMMM dd");
+            string assignment;
+            string purpose;
+
+            // variable assignment
+            WriteLine("What is the assignment number?");
+            assignment = ReadLine();
+            WriteLine("What is the purpose of this assignment?");
+            purpose = ReadLine();
+
+            // output
+            RepeatChar("#", bannerWidth);
+            WriteLine();
+
+            Write("# Programming Assignment #{0}", assignment);
+            RepeatChar(" ", 27 - assignment.Length);
+            WriteLine("#");
+
+            Write("# Developer: {0}", name);
+            RepeatChar(" ", 40 - name.Length);
+            WriteLine("#");
+
+            Write("# Date Submitted: {0} #", date);
+            RepeatChar(" ", 40 - date.Length);
+            WriteLine("#");
+
+            Write("# Purpose: {0} #", purpose);
+            RepeatChar(" ", 40 - purpose.Length);
+            WriteLine("#");
+
+            RepeatChar("#", 54);
+
+        }
+    }
+}
